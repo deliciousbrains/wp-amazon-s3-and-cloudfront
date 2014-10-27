e90d603b7908040c2bfdf8f52d074cad860ac92d
Iain <iain@polevaultweb.com>
2014-10-27 18:04:58 +0000

    Allow a string to be past to the error view

--- view/error.php
+++ view/error.php
@@ -1,7 +1,14 @@
 <div class="aws-content as3cf-error">
 
 	<div class="error">
-		<p><?php echo $error->get_error_message(); ?></p>
+		<p><?php
+			if ( isset( $error_message ) ) {
+				echo $error_message;
+			} else if ( isset( $error ) ) {
+				echo $error->get_error_message();
+			}
+			?>
+		</p>
 	</div>
 
 </div>
