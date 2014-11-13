479f836a7b28467d0a17bffdca1d070f381f26da
Iain <iain@polevaultweb.com>
2014-11-13 17:20:54 +0000

    Styling bucket list and UI tweaks for saving bucket. Show selected bucket and don't save if selected again to get back to settings.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,7 @@
 
 		$('.as3cf-settings').each(function() {
 			var $container = $(this);
-
-			if(!$container.hasClass('as3cf-has-bucket')){
-				loadBuckets();
-			}
-
+			
 			var $createBucketForm = $container.find('.as3cf-create-bucket-form');
 			if($createBucketForm.length){
 				var $createBucketButton = $createBucketForm.find('button'),
@@ -51,15 +47,24 @@
 			if($changeBucket.length){
 				$changeBucket.on('click', function(e){
 					e.preventDefault();
+					$( '.updated' ).hide();
 					$('.as3cf-settings').removeClass('as3cf-has-bucket');
+				});
+			}
+
+			var $refreshBuckets = $container.find('.as3cf-refresh-buckets');
+			if($refreshBuckets.length){
+				$refreshBuckets.on('click', function(e){
+					e.preventDefault();
 					loadBuckets();
 				});
 			}
 
 		});
 
+		var $bucketList = $('.as3cf-bucket-list');
+
 		function loadBuckets() {
-			var $bucketList = $('.as3cf-bucket-list');
 			$bucketList.html('<li class="loading">'+ $bucketList.attr('data-working') +'</li>');
 
 			var data = {
@@ -84,46 +89,60 @@
 						}
 
 						$(data['buckets']).each(function(idx, bucket){
-							$bucketList.append('<li><a href="#" data-bucket="'+ bucket.Name +'"><span class="dashicons dashicons-portfolio"></span> '+ bucket.Name +'</a></li>');
+							var bucket_class = ( bucket.Name == data['selected'] ) ? 'selected' : '';
+							$bucketList.append('<li><a class="' + bucket_class + '" href="#" data-bucket="'+ bucket.Name +'"><span class="bucket"><span class="dashicons dashicons-portfolio"></span> '+ bucket.Name +'</span><span class="spinner"></span></span></a></li>');
 						});
 					} else {
 						alert(as3cf_i18n.get_buckets_error + data['error']);
 					}
 				}
 			});
-
-			$bucketList.on('click', 'a', function(e){
-				e.preventDefault();
-				$bucketList.addClass('saving');
-				var bucketName = $(this).attr('data-bucket');
-
-				var data = {
-					action: 'as3cf-save-bucket',
-					bucket_name: bucketName,
-					_nonce: as3cf_i18n.save_bucket_nonce
-				};
-
-				$.ajax({
-					url:		ajaxurl,
-					type: 		'POST',
-					dataType: 	'JSON',
-					data: 		data,
-					error: function(jqXHR, textStatus, errorThrown) {
-						$bucketList.removeClass('saving');
-						alert(as3cf_i18n.save_bucket_error + errorThrown);
-					},
-					success: function(data, textStatus, jqXHR) {
-						$bucketList.removeClass('saving');
-						if (typeof data['success'] !== 'undefined') {
-							$('.as3cf-settings').addClass('as3cf-has-bucket');
-							$('.as3cf-active-bucket').text(bucketName);
-						} else {
-							alert(as3cf_i18n.save_bucket_error + data['error']);
-						}
-					}
-				});
-			});
 		}
+
+		$bucketList.on('click', 'a', function(e){
+			e.preventDefault();
+
+			if ( $(this).hasClass('selected') ) {
+				$('.as3cf-settings').addClass('as3cf-has-bucket');
+				return;
+			}
+
+			var bucket = this;
+			$('.as3cf-bucket-list a' ).removeClass('selected');
+			$(bucket).addClass('selected');
+
+			$bucketList.addClass('saving');
+			$(bucket).find('.spinner').show();
+			var bucketName = $(bucket).attr('data-bucket');
+
+			var data = {
+				action: 'as3cf-save-bucket',
+				bucket_name: bucketName,
+				_nonce: as3cf_i18n.save_bucket_nonce
+			};
+
+			$.ajax({
+				url:		ajaxurl,
+				type: 		'POST',
+				dataType: 	'JSON',
+				data: 		data,
+				error: function(jqXHR, textStatus, errorThrown) {
+					$bucketList.removeClass('saving');
+					alert(as3cf_i18n.save_bucket_error + errorThrown);
+				},
+				success: function(data, textStatus, jqXHR) {
+					$(bucket).find('.spinner').hide();
+					$bucketList.removeClass('saving');
+					if (typeof data['success'] !== 'undefined') {
+						$('.as3cf-settings').addClass('as3cf-has-bucket');
+						$('.as3cf-active-bucket').text(bucketName);
+						$( '.updated' ).show();
+					} else {
+						alert(as3cf_i18n.save_bucket_error + data['error']);
+					}
+				}
+			});
+		});
 
 	});
 
