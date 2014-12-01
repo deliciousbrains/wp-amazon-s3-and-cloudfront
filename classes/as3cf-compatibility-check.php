e1c63056ac0dfa6fc4886d14c9c2cee3c3d88699
Brad Touesnard <brad@touesnard.com>
2014-12-01 14:47:58 -0400

    Bug: Not showing notices on non-multisite.

--- classes/as3cf-compatibility-check.php
+++ classes/as3cf-compatibility-check.php
@@ -7,7 +7,8 @@ class AS3CF_Compatibility_Check {
 		$this->plugin_file_path = $plugin_file_path;
 		$this->aws_plugin_version_required = $aws_plugin_version_required;
 
-		add_action( 'network_admin_notices', array( $this, 'hook_network_admin_notices' ) );
+		add_action( 'admin_notices', array( $this, 'hook_admin_notices' ) );
+		add_action( 'network_admin_notices', array( $this, 'hook_admin_notices' ) );
 	}
 
 	function is_compatible() {
@@ -28,11 +29,11 @@ class AS3CF_Compatibility_Check {
 
 			if ( file_exists( WP_PLUGIN_DIR . '/amazon-web-services/amazon-web-services.php' ) ) {
 				$msg .= ' ' . __( 'It appears to be installed already.', 'as3cf' );
-				$activate_url = wp_nonce_url( self_admin_url( 'plugins.php?action=activate&amp;plugin=amazon-web-services/amazon-web-services.php' ), 'activate-plugin_amazon-web-services/amazon-web-services.php' );
+				$activate_url = wp_nonce_url( network_admin_url( 'plugins.php?action=activate&amp;plugin=amazon-web-services/amazon-web-services.php' ), 'activate-plugin_amazon-web-services/amazon-web-services.php' );
 				$msg .= ' <a style="font-weight:bold;text-decoration:none;" href="' . $activate_url . '">' . _x( 'Activate it now', 'Activate plugin', 'as3cf' ) . '</a>';
 			}
 			else {
-				$install_url = wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=amazon-web-services' ), 'install-plugin_amazon-web-services' );
+				$install_url = wp_nonce_url( network_admin_url( 'update.php?action=install-plugin&plugin=amazon-web-services' ), 'install-plugin_amazon-web-services' );
 				$msg .= sprintf( __( '<a href="%s">Install it</a> and activate.', 'as3cf' ), $install_url );
 			}
 
@@ -50,7 +51,7 @@ class AS3CF_Compatibility_Check {
 				$msg .= ' ' . sprintf( __( 'You currently have version %s installed.', 'as3cf' ), $aws_plugin_version );
 			}
 
-			$update_url = wp_nonce_url( self_admin_url( 'update.php?action=upgrade-plugin&plugin=amazon-web-services/amazon-web-services.php' ), 'upgrade-plugin_amazon-web-services/amazon-web-services.php' );
+			$update_url = wp_nonce_url( network_admin_url( 'update.php?action=upgrade-plugin&plugin=amazon-web-services/amazon-web-services.php' ), 'upgrade-plugin_amazon-web-services/amazon-web-services.php' );
 			$msg .= ' <a style="font-weight:bold;text-decoration:none;white-space:nowrap;" href="' . $update_url . '">' . __( 'Update to the latest version', 'as3cf' ) . '</a>';
 
 			$msg .= $hide_notice_msg;
@@ -65,7 +66,7 @@ class AS3CF_Compatibility_Check {
 			$msg = sprintf( __( 'Amazon S3 and CloudFront has been disabled because it will not work with the version of the Amazon&nbsp;Web&nbsp;Services plugin installed. Amazon&nbsp;S3&nbsp;and&nbsp;CloudFront %s or later is required.', 'as3cf' ), $as3cf_plugin_version_required );
 
 			$plugin_basename = plugin_basename( __FILE__ );
-			$update_url = wp_nonce_url( self_admin_url( 'update.php?action=upgrade-plugin&plugin=' . $plugin_basename ), 'upgrade-plugin_' . $plugin_basename );
+			$update_url = wp_nonce_url( network_admin_url( 'update.php?action=upgrade-plugin&plugin=' . $plugin_basename ), 'upgrade-plugin_' . $plugin_basename );
 			$msg .= ' <a style="font-weight:bold;text-decoration:none;white-space:nowrap;" href="' . $update_url . '">' . __( 'Update Amazon S3 and CloudFront to the latest version', 'as3cf' ) . '</a>';
 
 			$msg .= $hide_notice_msg;
@@ -77,7 +78,22 @@ class AS3CF_Compatibility_Check {
 		return $msg;
 	}
 
-	function hook_network_admin_notices() {
+	function hook_admin_notices() {
+		if ( is_multisite() ) {
+			if ( ! current_user_can( 'manage_network_plugins' ) ) {
+				return; // Don't show notices if the user can't manage network plugins
+			}
+		}
+		else {
+			// Don't show notices if user doesn't have plugin management privileges
+			$caps = array( 'activate_plugins', 'update_plugins', 'install_plugins' );
+			foreach ( $caps as $cap ) {
+				if ( ! current_user_can( $cap ) ) {
+					return;
+				}
+			}
+		}
+
 		$error_msg = $this->get_error_msg();
 
 		if ( ! $error_msg ) {
